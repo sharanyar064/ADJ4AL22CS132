76773124d2d59fecee4b82219c4a7eb9814626c2
sharanyar064 <rsharanya064@gmail.com>
2025-05-15 16:27:59 +0530

    Update PalindromeChecker.java

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,3 +1,6 @@
+///Write a Java Program for Checking if a string reads the same backward as forward (ignoring case 
+///and punctuation) using user defined function isPalindrome():
+
 package others;
 import java.util.Scanner;
 
